f613ee476dd3afe73a469b5506a9d000e1cad319
Jerry Yip <jerry.yip@gmail.com>
2021-02-04 14:00:39 -0800

    feat: initial UI render of all API response data

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -108,69 +108,192 @@ const handleSubmit = async(event) => {
             }
             else destination = `${res.city}, ${res.country}`;
 
-            let tripHolder = document.createElement("div");
-            tripHolder.className = "summary-wrapper";
             let newTrip = `
+            <div class="summary-wrapper">
             <div class="trip-summary">
             <div class="trip-image">
             <img class = "trip-photo" src="${res.image}" alt="trip image">
-       </div>
-            <div class="modal-details">
-                <h2 class="locale">${destination}</h2>
-                <h3 class="dates">Depart:&nbsp<span id="depart-date">${dateString(start_dt)} (${dayOfWeek(start_dt)})</span></h3>
-                <h3 class="dates">Return:&nbsp<span id="return-date">${dateString(end_dt)} (${dayOfWeek(end_dt)})</span></h3>
-                <h3>Local Time:&nbsp<span class="local-time" data-${destination}>${localDateTime(res.timeZone)}</span></h3>
-                <h3><span class="countdown">${daysLeft(start_dt)} days until your trip</h3>
-                <div class="btn-group">
-                    <button class="trip-btn">Save Trip</button>
-                    <button class="trip-btn">Delete Trip</button>
-                </div>
             </div>
-        </div>  
-            `
-            tripHolder.innerHTML = newTrip;
-            const modalContainer = document.querySelector(".modal-content");
-            modalContainer.prepend(tripHolder); 
-/*
-
-            let dailyForecasts = document.createElement("div");
-            dailyForecasts.className = "daily-forecasts";
+                    <div class="modal-details">
+                        <h2 class="locale">${destination}</h2>
+                        <h3 class="dates">Depart:&nbsp<span id="depart-date">${dateString(start_dt)} (${dayOfWeek(start_dt)})</span></h3>
+                        <h3 class="dates">Return:&nbsp<span id="return-date">${dateString(end_dt)} (${dayOfWeek(end_dt)})</span></h3>
+                        <h3>Local Time:&nbsp<span class="local-time" data-${destination}>${localDateTime(res.timeZone)}</span></h3>
+                        <h3><span class="countdown">${daysLeft(start_dt)} days until your trip</h3>
+                        <div class="btn-group">
+                            <button class="trip-btn">Save Trip</button>
+                            <button class="trip-btn">Delete Trip</button>
+                        </div>
+                    </div>
+                </div>  
+            </div> 
+            `;
 
             let dt = new Date(`${start_dt} 00:00:00`);
             let endDt = new Date(`${end_dt} 00:00:00`);
-            while (dt.getTime() <= endDt.getTime()) {
 
-                let dayForecast = document.createElement("div");
-                dayForecast.className = "day-forecast";
-                console.log("date counter:", dt);
+            let dailyDetail = `
+            <div class="trip-daily-detail">
+            <div class="trip-daily-container">
+                <div class="trip-daily-wrapper">
+                    <div class="forecast-container" id="sticky-weather">
+                        <div id="forecast-header">
+                            <h3>Weather Forecast &nbsp</h3>
+                            <div class="weather-details">
+                                <button class="expander">[ Details
+                                </button>
+                            </div>
+                        </div>
+                        <div class="daily-forecasts" id="div1">
+                    `;
+            let itineraryHeader = `
+                </div>
+                <div class="itinerary-header">
+                    <h3>Itinerary</h3>
+                    <h4 class="cat-fill">
+                        [ Categories ]
+                    </h4>
+                </div>
+            </div>
+            <div class="itinerary-schedule">
+                <div class="itinerary-timeline">7 AM <span class="sched-expand">&#9650</span></div>
+                <div class="itinerary-timeline">8 AM</div>
+                <div class="itinerary-timeline">9 AM</div>
+                <div class="itinerary-timeline">10 AM</div>
+                <div class="itinerary-timeline">11 AM</div>
+                <div class="itinerary-timeline">12 PM</div>
+                <div class="itinerary-timeline">1 PM</div>
+                <div class="itinerary-timeline">2 PM</div>
+                <div class="itinerary-timeline">3 PM</div>
+                <div class="itinerary-timeline">4 PM</div>
+                <div class="itinerary-timeline">5 PM</div>
+                <div class="itinerary-timeline">6 PM</div>
+                <div class="itinerary-timeline">7 PM</div>
+                <div class="itinerary-timeline">8 PM</div>
+                <div class="itinerary-timeline">9 PM</div>
+                <div class="itinerary-timeline">10 PM</div>
+                <div class="itinerary-timeline">11 PM &#9660</div>
+            </div>
+            `;
+            let dailyForecasts = `
+                            <div class="day-forecast">
+                    <div class="day-border">
+                        <h4 class="forecast-date">Thu 10/1</h4>
+                        <img class="weather-icon" src="../media/weather_icons/a01d.png"
+                            alt="few clouds">
+                        <div class="weather-desc">Few Clouds</div>
+                        <div class="high-low">81&#176<span class="temp-divider"> |
+                            </span>69&#176&nbsp
+                            <img class="precip-icon" src="../media/droplet2.png"
+                                alt="precipitation probability">
+                            <span class="precip-prob">1%</span>
+                        </div>
+                        <div class="sunrise">Sunrise: 7:01AM</div>
+                        <div class="sunset">Sunset: 6:09 PM</div>
+                    </div>
+                </div>
+                `;
+            let dailyItineraries = `
+            <div class="itinerary-container">
+            <div class="itinerary-wrapper" id="div2">
+                <div class="itinerary-input">
+                    <div class="textareaElement food" contenteditable>Pack breakfast for flight
+                        (example)</div>
+                    <div class="textareaElement logistics" contenteditable>7:30am Uber to ____
+                        airport (example)</div>
+                    <div class="textareaElement logistics" contenteditable>9:15am 2 hr flight to
+                        _____ (example)</div>
+                    <div class="textareaElement logistics" contenteditable></div>
+                    <div class="textareaElement logistics" contenteditable>Pick up rental @___ +
+                        drive to downtown (example)</div>
+                    <div class="textareaElement food" contenteditable>12pm Lunch resos @____
+                        (example) </div>
+                    <div class="textareaElement activity" contenteditable>Explore downtown and
+                        views @______ (example)</div>
+                    <div class="textareaElement activity" contenteditable></div>
+                    <div class="textareaElement logistics" contenteditable>Check-in to hotel
+                        @_____ (example)</div>
+                    <div class="textareaElement activity" contenteditable>2 hr hike around_____
+                        (example)</div>
+                    <div class="textareaElement activity" contenteditable></div>
+                    <div class="textareaElement food" contenteditable>6:30 Dinner resos @_____
+                        (example)</div>
+                    <div class="textareaElement food" contenteditable></div>
+                    <div class="textareaElement entertainment" contenteditable>8pm Tix to Show
+                        @_____ (example)</div>
+                    <div class="textareaElement entertainment" contenteditable></div>
+                    <div class="textareaElement" contenteditable></div>
+                    <div class="textareaElement" contenteditable></div>
+                </div>
+                `;
 
+            while (dt.getTime() < endDt.getTime()) {
+                //console.log("date counter:", dt);
                 let dateStr = (dt.getMonth() + 1) + '/' + dt.getDate();
-                console.log("dateStr counter:", dateStr);
-                
+                //console.log("dateStr counter:", dateStr);
                 let dateStr2 = (dt.getFullYear()+'-'+ ('0' + (dt.getMonth() + 1)).slice(-2) + '-' + ('0' + dt.getDate()).slice(-2));
-                console.log("dateStr2 counter:", dateStr2);
+                //console.log("dateStr2 counter:", dateStr2);
 
-                dayForecast.innerHTML = `
-                <div class="day-border">
-                <h4 class="forecast-date">${dayOfWeek(dateStr2)} ${dateStr}</h4>
-                <img class="weather-icon" src="https://www.weatherbit.io/static/img/icons/${res.weather[dateStr2].icon}.png" alt="weather icon">
-                <div class="weather-desc">${res.weather[dateStr2].description}</div>
-                <div class="high-low">${res.weather[dateStr2].high}&#176<span class="temp-divider"> | </span>${res.weather[dateStr2].low}&#176&nbsp
-                    <img class="precip-icon" src="${droplet2}" alt="precipitation probability">
-                    <span class="precip-prob">1%</span>
+                let dayForecast = `
+                <div class="day-forecast">
+                    <div class="day-border">
+                        <h4 class="forecast-date">Thu 10/1</h4>
+                        <img class="weather-icon" src="../media/weather_icons/a01d.png"
+                            alt="few clouds">
+                        <div class="weather-desc">Few Clouds</div>
+                        <div class="high-low">81&#176<span class="temp-divider"> |
+                            </span>69&#176&nbsp
+                            <img class="precip-icon" src="../media/droplet2.png"
+                                alt="precipitation probability">
+                            <span class="precip-prob">1%</span>
+                        </div>
+                        <div class="sunrise">Sunrise: 7:01AM</div>
+                        <div class="sunset">Sunset: 6:09 PM</div>
+                    </div>
                 </div>
-                <div class="sunrise">Sunrise: 7:01AM</div>
-                <div class="sunset">Sunset: 6:09 PM</div>
+                `;
+                dailyForecasts = dailyForecasts + dayForecast;
+
+                let dayItinerary = `
+                    <div class="itinerary-input">
+                    <div class="textareaElement" contenteditable></div>
+                    <div class="textareaElement" contenteditable></div>
+                    <div class="textareaElement" contenteditable></div>
+                    <div class="textareaElement" contenteditable></div>
+                    <div class="textareaElement" contenteditable></div>
+                    <div class="textareaElement" contenteditable></div>
+                    <div class="textareaElement" contenteditable></div>
+                    <div class="textareaElement" contenteditable></div>
+                    <div class="textareaElement" contenteditable></div>
+                    <div class="textareaElement" contenteditable></div>
+                    <div class="textareaElement" contenteditable></div>
+                    <div class="textareaElement" contenteditable></div>
+                    <div class="textareaElement" contenteditable></div>
+                    <div class="textareaElement" contenteditable></div>
+                    <div class="textareaElement" contenteditable></div>
+                    <div class="textareaElement" contenteditable></div>
+                    <div class="textareaElement" contenteditable></div>
                 </div>
-                `
-                
-                dailyForecasts.appendChild(dayForecast);
+                `;
+                dailyItineraries = dailyItineraries + dayItinerary;
+
                 dt.setDate(dt.getDate() + 1);
             }
-            tripHolder.parentNode.insertBefore(dailyForecasts, tripHolder.nextSibling);
-            //tripHolder.append(dailyForecasts);
-            //console.log(tripHolder);
-*/
+            let closer = `
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            `;
+
+            /*let tripHolder = document.createElement("div");
+            tripHolder.className = "summary-wrapper";*/
+            newTrip = newTrip + dailyDetail + dailyForecasts + itineraryHeader + dailyItineraries + closer;
+            console.log(newTrip);
+            //tripHolder.innerHTML = newTrip;
+            const modalContainer = document.querySelector(".modal-content");
+            modalContainer.innerHTML = newTrip; 
         })
 
     }
